b01e9ec67f205f4781eb12600a0a12610d26f4c2
Santiago Higuera <shiguera@mercatorlab.com>
2014-02-11 13:44:08 +0100

    Add lists of LocationListener and GpsStatus.Listener to GpsManager

--- src/com/mlab/android/gpsmanager/GpsManager.java
+++ src/com/mlab/android/gpsmanager/GpsManager.java
@@ -4,11 +4,11 @@ package com.mlab.android.gpsmanager;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
 
 import android.content.Context;
 import android.location.GpsSatellite;
 import android.location.GpsStatus;
-import android.location.GpsStatus.Listener;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -25,49 +25,85 @@ import android.util.Log;
  * se utiliza el método 'registerGpsListenerer()'. También se dispone de un
  * método 'unregisterGpsListenerer()'.<p>
  * Para utilizar el GpsManager hay que instanciarle a través del constructor
- * de la clase, que recibe como parámetro el Context de la Activity que le instancia.<p>
+ * de la clase, que recibe como parámetro el Context de la Activity que le instancia. 
+ * El Context lo necesita para acceder a los servicios del sistema</p>
  * Una vez instanciado hay que comprobar con el método 'isGpsEnabled()' que el GPS se ha activado 
  * correctamente.<p>
- * Para iniciar las actualizaciones hay que llamar al  método '<em>startGpsUpdates()</em>'<p>
- * Para detener las actualizaciones se utiliza el método <em>'stopGpsUpdates()'</em>
+ * <p>La clase que utiliza GpsManager debe implementar el interface GpsListener que 
+ * impone tener los métodos 'updateLocation()' y 'firstFixEvent()'. 
+ * Esta clase, tras instanciar el GpsManager debera registrarse como listener con 
+ * el método 'GpsManager.registerGpsListener()'.<p>
+ * <p>Para iniciar las actualizaciones hay que llamar al  método '<em>startGpsUpdates()</em>'</p>
+ * <p>Para detener las actualizaciones se utiliza el método <em>'stopGpsUpdates()'</em></p>
+ * 
  * @author shiguera
  *
  */
-public class GpsManager {
+public class GpsManager implements LocationListener, GpsStatus.Listener {
 
 	// FIXME trabajar en un thread secundario
 	// FIXME Habilitar la coarse-location
+	
+	private final long MIN_TIME = 50;
+	private final float MIN_DISTANCE = 1.0f;
 	
 	private Context context;
 	/**
 	 * Lista de listeners del GpsManager
 	 */
-	private ArrayList<GpsListener> gpsListeners;
+	private List<GpsListener> gpsListeners;
+	/**
+	 * Lista de LocationListener registrados para escuchar los eventos.
+	 * 
+	 */
+	private List<LocationListener> locationListeners;
+	/**
+	 * Lista de GpsStatus.Listener registrados
+	 */
+	private List<GpsStatus.Listener> statusListeners;
 	
-	// locationManager
+	/**
+	 *  locationManager
+	 */
 	private LocationManager locationManager;
-	//private LocationProvider locationProvider;	
-	// minTime : the minimum time interval for notifications, in milliseconds. 
-	// This field is only used as a hint to conserve power, 
-	// and actual time between location updates may be greater or lesser than this value.
+	private String locationProvider;	
+	
+	/**
+	 *  minTime : the minimum time interval for notifications, in milliseconds. 
+	 *
+	 * This field is only used as a hint to conserve power, 
+	 * and actual time between location updates may be greater or lesser than this value.
+	*/
 	private long minTime;
 	
-	// minDistance : the minimum distance interval for notifications, in meters
+	/**
+	 *  minDistance : the minimum distance interval for notifications, in meters
+	 */
 	private float minDistance;
 	
-	// gpsEventFirstFix : Indica si ya se ha producido el evento FIRST_FIX
+	/**
+	 *  gpsEventFirstFix : Indica si ya se ha producido el evento FIRST_FIX
+	 */
 	private boolean gpsEventFirstFix;
 	
-	// lastLocation: Almacena el último objeto location leido del GPS
+	/**
+	 *  lastLocation: Almacena el último objeto location leido del GPS
+	 */
 	private Location lastLocation;
 
-	// gpsStatus: Almacena el ultimo objeto GpsStatus leido del GPS
+	/**
+	 *  gpsStatus: Almacena el ultimo objeto GpsStatus leido del GPS
+	 */
 	private GpsStatus gpsStatus;
 	
-	// gpsSatellite: Lista de satelites utilizada en el último location (lastLocation)
-	private ArrayList<GpsSatellite> gpsSatellite; 
+	/**
+	 *  gpsSatellite: Lista de satelites utilizada en el último location (lastLocation)
+	 */
+	private List<GpsSatellite> gpsSatellite; 
 	
-	// numpuntos: Número de puntos leidos desde que se activó el GPS
+	/**
+	 *  numpuntos: Número de puntos leidos desde que se activó el GPS
+	 */
 	private int numpuntos;
 	
 	
@@ -77,17 +113,41 @@ public class GpsManager {
 	 */
 	public GpsManager(Context ctxt) {
 		Log.d("HAL","GpsManager.builder()");
-		this.gpsListeners= new ArrayList<GpsListener>();
-		
+	
 		this.context=ctxt;
-		this.minTime=10;
-		this.minDistance=10.0f;
-		this.initGps();
-		lastLocation=new Location(LocationManager.GPS_PROVIDER);
+		this.minTime = this.MIN_TIME;
+		this.minDistance = this.MIN_DISTANCE;
+		this.locationProvider = LocationManager.GPS_PROVIDER;
+		
 		gpsSatellite=new ArrayList<GpsSatellite>();
+		// Lista de listeners del interface GpsListener
+		gpsListeners= new ArrayList<GpsListener>(); 
+		// Lista de listeners del interface LocationListener
+		locationListeners = new ArrayList<LocationListener>();
+		registerLocationListener(this);
+		// Lista de listeners del interface GpsStatus.Listener
+		statusListeners = new ArrayList<GpsStatus.Listener>();
+		this.registerStatusListener(this);
+		
+		this.initGps();		
+		lastLocation=new Location(LocationManager.GPS_PROVIDER);
+
 		gpsEventFirstFix=false;
 	}		
 
+	// Gestion de listas de listeners
+	public boolean registerLocationListener(LocationListener locationListener) {
+		return this.locationListeners.add(locationListener);
+	}
+	public boolean removeLocationListener(LocationListener locationListener) {
+		return this.locationListeners.remove(locationListener);
+	}
+	public boolean registerStatusListener(GpsStatus.Listener statusListener) {
+		return this.statusListeners.add(statusListener);
+	}
+	public boolean removeStatusListener(GpsStatus.Listener statusListener) {
+		return this.statusListeners.remove(statusListener);
+	}
 	/**
 	 * Añade el GpsController a la lista de los notificados por cambio de posición
 	 * @param gpsController
@@ -110,7 +170,8 @@ public class GpsManager {
 	 */
 	private boolean initGps() {
         Log.d("HAL","GpsManager.initGps()");
-		this.locationManager = (LocationManager) (this.context.getSystemService(Context.LOCATION_SERVICE));
+		this.locationManager = 
+			(LocationManager)(this.context.getSystemService(Context.LOCATION_SERVICE));
         if (isGpsEnabled()) {
         	//this.locationProvider = this.locationManager.getProvider(LocationManager.GPS_PROVIDER);       	
         	Log.d("HAL","GpsManager.initGps(): isGpsEnabled=true");
@@ -128,7 +189,7 @@ public class GpsManager {
 	 */
 	public boolean isGpsEnabled() {
         try {
-        	return this.locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        	return locationManager.isProviderEnabled(locationProvider);
         } catch (Exception e) {
         	return false;
         }
@@ -157,62 +218,44 @@ public class GpsManager {
 			initGps();
 		}
 		if (isGpsEnabled()) {
-        	this.locationManager.addGpsStatusListener(this.gpsStatusListener);
-            //Log.d("HAL", "    GpsStatusListener asignado");
-        	this.locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, this.minTime, 
-        			this.minDistance, this.locationListener);
-            //Log.d("HAL", "    GpsManager: requestLocationUpdates started");
-        	return true;
+			addListenersToLocationManager();
+			return true;
         } else {
         	return false;
         }
 	}
-
+	private void addListenersToLocationManager() {
+		if(this.locationManager != null) {
+			for(LocationListener ll: locationListeners) {
+				this.locationManager.requestLocationUpdates(
+					locationProvider, minTime, minDistance, ll);
+			}
+			for(GpsStatus.Listener sl: statusListeners) {
+				this.locationManager.addGpsStatusListener(sl);
+			}
+		}
+	}
 	/**
 	 * Detiene las actualizaciones de la posición del Gps
 	 */
 	public void stopGpsUpdates() {
 		// Detener actualizaciones del GPS
 		Log.d("HAL","GpsManager.stopGpsUpdates()");
-		if (this.locationManager != null) {
-			//Log.d("HAL", "    GpsManager: Removing updates");
-			this.locationManager.removeUpdates(this.locationListener);
-			//Log.d("HAL", "    GpsManager: Removed locationListener");
-			this.locationManager.removeGpsStatusListener(this.gpsStatusListener);
-			//Log.d("HAL", "    GpsManager: Removed gpsStatusListener");			
+		if (locationManager != null) {
+			removeListenersFromLocationManager();
 		}
 		this.gpsSatellite = new ArrayList<GpsSatellite>();
 	}
-	
-	//
-	// gpsStatusListener
-	//
-	Listener gpsStatusListener = new GpsStatus.Listener() {		
-		
-		@Override
-		public void onGpsStatusChanged(int event) {
-			String text="";
-			switch(event) {
-			case(GpsStatus.GPS_EVENT_SATELLITE_STATUS):
-				text="GPS_EVENT_SATELLITE_STATUS";
-				GpsStatus status = locationManager.getGpsStatus(null);
-				updateGpsStatus(status);				
-				break;
-			case(GpsStatus.GPS_EVENT_STARTED):
-				text="GPS_EVENT_STARTED";
-				break;
-			case(GpsStatus.GPS_EVENT_STOPPED):
-				text="GPS_EVENT_STOPED";
-				break;
-			case(GpsStatus.GPS_EVENT_FIRST_FIX):
-				text="GPS_EVENT_FIRST_FIX";
-				firstFixEvent();				
-				break;
+	private void removeListenersFromLocationManager() {
+		if(this.locationManager != null) {
+			for(LocationListener ll: this.locationListeners) {
+				this.locationManager.removeUpdates(ll);
 			}
-			//Log.d("HAL", "GpsManager.onGpsStatusChanged(): "+text);
+			for(GpsStatus.Listener sl: this.statusListeners) {
+				this.locationManager.removeGpsStatusListener(sl);
+			}
 		}
-	};
-	
+	}
 	// updateGpsStatus()
 	//        Actualiza los valores de gpsStatus, satelites y otros
 	private void updateGpsStatus(GpsStatus status) {
@@ -230,7 +273,7 @@ public class GpsManager {
 	 * Este método es llamado cada vez que el LocationManager actualiza 
 	 * la posición en el método 'onLocationChanged()' de su LocationListener.<p>
 	 * El método actualiza las variables 'lastLocation' y 'numpuntos' y
-	 * luego notifica a todos los GpsController regitrados llamando a sus métodos
+	 * luego notifica a todos los GpsListeners regitrados llamando a sus métodos
 	 * 'updateLocation()'<p>
 	 * 
 	 * @param loc Ultima actualización de Location proporcionada por el LocationManager 
@@ -254,43 +297,6 @@ public class GpsManager {
         	this.gpsListeners.get(i).firstFixEvent();
         }
 	}
-	
-	/**
-	 * LocationListener del LocationManager que recibe las notificaciones de nuevas posiciones
-	 */
-	LocationListener locationListener = new LocationListener() {
-	    @Override
-		public void onLocationChanged(Location loc) {
-	    	Log.d("HAL","LocationListener.onLocationChanged():\n");
-	    	updateLocation(loc);
-	    }
-	    @Override
-		public void onStatusChanged(String provider, int status, Bundle extras) {
-	    	switch(status) {
-	    	case(LocationProvider.AVAILABLE):
-	    		Log.d("HAL","LocationListener.onStatusChanged(): LocationProvider.AVAILABLE");
-	    		
-	    		break;
-	    	case(LocationProvider.TEMPORARILY_UNAVAILABLE):
-	    		Log.d("HAL","LocationListener.onStatusChanged(): LocationProvider.TEMPORARILY_UNAVAILABLE");
-
-	    		break;
-	    	case(LocationProvider.OUT_OF_SERVICE):
-	    		Log.d("HAL","LocationListener.onStatusChanged(): LocationProvider.OUT_OF_SERVICE");
-	    		
-	    		break;
-	    	}
-	    	//Toast.makeText(getApplicationContext(), "Status Changed", 2000).show();
-	    }
-	    @Override
-		public void onProviderEnabled(String provider) {
-	    	Log.d("HAL","LocationListener.onProviderEnabled()");
-	    }	
-	    @Override
-		public void onProviderDisabled(String provider) {
-	    	Log.d("HAL","LocationListener.onProviderDisabled()");
-	    }
-	};
 	
 	public long getMinTime() {
 		return minTime;
@@ -316,5 +322,61 @@ public class GpsManager {
 	public int getNumpuntos() {
 		return numpuntos;
 	}
-	
+
+	// Interface GpsStatus.Listener
+	@Override
+	public void onGpsStatusChanged(int event) {
+		String text="";
+		switch(event) {
+		case(GpsStatus.GPS_EVENT_SATELLITE_STATUS):
+			text="GPS_EVENT_SATELLITE_STATUS";
+			GpsStatus status = locationManager.getGpsStatus(null);
+			updateGpsStatus(status);				
+			break;
+		case(GpsStatus.GPS_EVENT_STARTED):
+			text="GPS_EVENT_STARTED";
+			break;
+		case(GpsStatus.GPS_EVENT_STOPPED):
+			text="GPS_EVENT_STOPED";
+			break;
+		case(GpsStatus.GPS_EVENT_FIRST_FIX):
+			text="GPS_EVENT_FIRST_FIX";
+			firstFixEvent();				
+			break;
+		}
+		//Log.d("HAL", "GpsManager.onGpsStatusChanged(): "+text);		
+	}
+
+	// Interface LocationListener
+    @Override
+	public void onLocationChanged(Location loc) {
+    	Log.d("HAL","LocationListener.onLocationChanged():\n");
+    	updateLocation(loc);
+    }
+    @Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+    	switch(status) {
+    	case(LocationProvider.AVAILABLE):
+    		Log.d("HAL","LocationListener.onStatusChanged(): LocationProvider.AVAILABLE");
+    		
+    		break;
+    	case(LocationProvider.TEMPORARILY_UNAVAILABLE):
+    		Log.d("HAL","LocationListener.onStatusChanged(): LocationProvider.TEMPORARILY_UNAVAILABLE");
+
+    		break;
+    	case(LocationProvider.OUT_OF_SERVICE):
+    		Log.d("HAL","LocationListener.onStatusChanged(): LocationProvider.OUT_OF_SERVICE");
+    		
+    		break;
+    	}
+    	//Toast.makeText(getApplicationContext(), "Status Changed", 2000).show();
+    }
+    @Override
+	public void onProviderEnabled(String provider) {
+    	Log.d("HAL","LocationListener.onProviderEnabled()");
+    }	
+    @Override
+	public void onProviderDisabled(String provider) {
+    	Log.d("HAL","LocationListener.onProviderDisabled()");
+    }
 }
